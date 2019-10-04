cdd537d95da0800f27196ccd819b886c6298489d
Travis Vachon <travis.vachon@gmail.com>
2019-10-04 16:51:50 -0700

    use a named node for the belief container link

--- src/components/Install/index.js
+++ src/components/Install/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { useLDflexValue } from '@solid/react'
+import { namedNode } from '@rdfjs/data-model';
 
 import data from '@solid/query-ldflex';
 
+window.data = data
 
 export default () => {
   const install = async () => {
     const webId = (await data.user).value
-    await data.user.hasBeliefs.set(new URL("/beliefs", webId).toString())
+    await data.user.hasBeliefs.set(namedNode(new URL("/beliefs", webId).toString()))
   }
   const installedLd = useLDflexValue(`user.hasBeliefs`)
   const installed = installedLd && installedLd.value
-
-  if (!installed) {
-    return <button onClick={install}>Install</button>
+  return (<button onClick={install}>Install</button>)
+/*  if (!installed) {
+    return
   } else {
     return "INSTALLED!"
-  }
+  }*/
 }
