1ae84119974e5cdf7b7d4c1758180dfe8f721a19
Travis Vachon <travis.vachon@gmail.com>
2019-10-19 23:15:32 -0700

    Add claims.json generated from ohhey args, plus tools to upload it

--- src/components/Install/index.js
+++ src/components/Install/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { useLDflexValue } from '@solid/react'
 import { namedNode } from '@rdfjs/data-model';
-
+import claims from '../../claims.json'
 import data from '@solid/query-ldflex';
+import {createDocument} from '../../utils/ldflex-helper'
 
 window.data = data
 
 export default () => {
   const install = async () => {
     const webId = (await data.user).value
-    await data.user.hasBeliefs.set(namedNode(new URL("/beliefs", webId).toString()))
+    const root = new URL("/beliefs/", webId)
+    for (var slug in claims) {
+      createDocument(
+        (new URL(slug, root)).toString(),
+        claims[slug]
+      )
+    }
+    //console.log(new URL("/beliefs", webId).toString())
+
+    //console.log(namedNode(new URL("/beliefs", webId).toString()))
+    //await data.user.hasBeliefs.set(namedNode(new URL("/beliefs", webId).toString()))
   }
   const installedLd = useLDflexValue(`user.hasBeliefs`)
   const installed = installedLd && installedLd.value
