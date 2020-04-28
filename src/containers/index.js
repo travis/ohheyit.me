8b49f4e67b89c795111c3abc654f6c8d6b743c3d
Travis Vachon <travis.vachon@gmail.com>
2020-04-27 19:09:09 -0700

    new gatsby site

--- src/containers/index.js
+++
@@ -1,8 +0,0 @@
-import Login from './Login';
-import Register, { RegistrationSuccess } from './Register';
-import PageNotFound from './PageNotFound';
-import Welcome from './Welcome';
-import Profile from './Profile';
-import Belief from './Belief';
-
-export { Login, Register, RegistrationSuccess, PageNotFound, Welcome, Profile, Belief };
--- src/slug2WebId.js
+++
@@ -1,6 +0,0 @@
-const slugs2WebIds = {
-    travis: "https://tvachon.inrupt.net/profile/card#me",
-    toby: "https://tobytoberson.inrupt.net/profile/card#me"
-}
-
-export default (slug) => slugs2WebIds[slug]
