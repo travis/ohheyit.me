88f9a4fdee727a7880887053e819e1ef69692f7a
Travis Vachon <travis.vachon@gmail.com>
2020-04-29 17:37:45 -0700

    really basic landing page

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,11 +8,19 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { makeStyles } from "@material-ui/core/styles"
 
 import Header from "./header"
-import "./layout.css"
+
+const useStyles = makeStyles({
+  footer: {
+    textAlign: "center"
+  }
+})
 
 const Layout = ({ children }) => {
+  const classes = useStyles()
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -25,21 +33,12 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <CssBaseline/>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <main>{children}</main>
+      <footer className={classes.footer}>
+        © {new Date().getFullYear()} tvcorp
+      </footer>
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,53 @@
 import React from "react"
 import { Link } from "gatsby"
+import { createMuiTheme, makeStyles, ThemeProvider } from "@material-ui/core/styles"
+import Button from "@material-ui/core/Button"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const useStyles = makeStyles(theme => ({
+  imageContainer: {
+    width: "100%",
+    padding: theme.spacing(10),
+    position: "relative"
+  },
+  controlsContainer: {
+    position: "absolute",
+    top: theme.spacing(6),
+    bottom: 0,
+    left: 0,
+    right: 0,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "stretch"
+  },
+  interestButton: {
+    textAlign: "center"
+  }
+}))
+
+const theme = createMuiTheme({
+  spacing: 9
+});
+
+const IndexPage = () => {
+  const classes = useStyles()
+  return (
+    <Layout>
+      <SEO title="itme" />
+      <div className={classes.imageContainer}>
+        <Image />
+        <div className={classes.controlsContainer}>
+          <Button className={classes.interestButton} href="https://tinyletter.com/itme">
+            get updates
+          </Button>
+        </div>
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
